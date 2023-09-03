ce52bd888e666d8fdda2bc0f9df888270dd09949
yoGhastly <ve06976@innovaccion.mx>
2023-09-03 17:06:38 -0600

    send data from report file

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 
 import { loadApiEndpoints } from "./controllers/api";
 
@@ -10,8 +11,16 @@ const app = express();
 app.set("port", process.env.PORT ?? 3000);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 }));
+app.use((_req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+app.use(cors());
+app.use(
+  express.static(path.join(__dirname, "../public"), { maxAge: 31557600000 }),
+);
 
 loadApiEndpoints(app);
 
--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,9 +1,24 @@
 import { Application, Request, Response } from "express";
 
-import CoursesData from "../../data/courses.json";
+import path from "path";
+import fs from "fs";
 
 export const loadApiEndpoints = (app: Application): void => {
-	app.get("/api", (req: Request, res: Response) => {
-		return res.status(200).send(CoursesData);
-	});
+  app.get("/", (_req, res) => {
+    res.status(200).send("Hello world!");
+  });
+
+  app.post("/api", (req, res) => {
+    const filePath = path.join(__dirname, "../../data/report.txt");
+
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        console.error("Error reading file:", err);
+        res.status(500).send("Error reading file");
+      } else {
+        res.set("Content-Type", "text/plain");
+        res.send(data);
+      }
+    });
+  });
 };
