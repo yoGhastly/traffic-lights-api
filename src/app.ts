b9ee16a9e6d49a74d30b98d0cd79683d4961140f
yoGhastly <ve06976@innovaccion.mx>
2023-09-03 19:17:22 -0600

    socket.io added to server

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
-
 import { loadApiEndpoints } from "./controllers/api";
 
 // Create Express server
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,41 @@
 /* eslint-disable no-console */
+import { Server } from "socket.io";
 import app from "./app";
 
 /**
  * Start Express server.
  */
 const server = app.listen(app.get("port"), () => {
-	console.log(
-		"  App is running at http://localhost:%d in %s mode",
-		app.get("port"),
-		app.get("env"),
-	);
-	console.log("  Press CTRL-C to stop\n");
+  console.log(
+    "  App is running at http://localhost:%d in %s mode",
+    app.get("port"),
+    app.get("env"),
+  );
+  console.log("  Press CTRL-C to stop\n");
+});
+
+export const io = new Server(server, {
+  cors: {
+    origin: ["www.piesocket.com", "http://localhost:8081"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Access-Control-Allow-Origin"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Listen for incoming messages from clients
+  socket.on("message", (message) => {
+    // Broadcast the message to all connected clients
+    io.emit("message", message);
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
 });
 
 export default server;
